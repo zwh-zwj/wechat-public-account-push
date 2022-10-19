fbc48a991fb172e08b76e9b2a5da0b050a8d98d1
zwh-zwj <116145003+zwh-zwj@users.noreply.github.com>
2022-10-19 16:37:38 +0800

    Update server-config.js

--- config/server-config.js
+++ config/server-config.js
@@ -1,4 +1,67 @@
-// 此时间为每天的早上8点，*为匹配任意一个
-// 这里的时间是中国时间 秒 分 时 日 月 年
-const cornTime = '0 0 8 * * *'
-export default cornTime
+/* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
+const USER_CONFIG = {
+
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: 'wx13ccdbaae34a2594',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: 'cdc5bab8fc5e3bae1752d5f18929f050',
+
+  PROVINCE: '',
+  CITY: '驻马店',
+
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oBgWL5rztaMHX_7iuj95kkKOMMcA',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'OaFqRTtCk9fLBros1nOF2TRtYzJReNtCPDRiH6mBOHQ',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '2001', date: '02-14',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '1996', date: '09-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-05-21' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: '',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: '',
+    }
+  ],
+
+}
+
+module.exports = USER_CONFIG
+
