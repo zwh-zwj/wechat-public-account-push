b52a2fd26dd7fd93197c98ec0ef6f60d02986451
zwh-zwj <116145003+zwh-zwj@users.noreply.github.com>
2022-10-19 16:08:35 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -6,15 +6,15 @@ const TEMPLATE_CONFIG = [
     desc: `
       **{{date.DATA}}**
       
-      下个休息日：{{holidaytts.DATA}}
       
-      ---
+
       
       城市：{{city.DATA}}
       
       天气：{{weather.DATA}}
       
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      最高气温:{{max_temperature.DATA}} 
+      最低气温{{min_temperature.DATA}}
       
       风向: {{wind_direction.DATA}}
       
@@ -22,9 +22,9 @@ const TEMPLATE_CONFIG = [
       
       {{comprehensive_horoscope.DATA}}
       
-      ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      
+      今天是我们相恋的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
